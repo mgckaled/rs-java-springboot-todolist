2c05e5af00aaccd529b8585de7d477da8c020b9c
mgckaled <marcelgckaled@gmail.com>
2023-10-10 11:30:42 -0300

    first commit

---
+++ src/test/java/br/com/mgckdev/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mgckdev.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
